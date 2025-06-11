58d42c9c0565a6ef24a7439a531e59945b744171
sabrina/702 <sabrinassgs101@gmail.com>
2025-06-11 20:08:19 -0300

    Teste Responsividade

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
 import React from 'react'
 import SideBar from './components/Sidebar/Sidebar'
 import Painel from './components/Painel/Painel'
+import './App.css'
 
 function App() {
 
   return (
     <>
+    <div className="app-wrapper">
       <SideBar />
       <Painel />
-      
+    </div>
     </>
   )
 }
